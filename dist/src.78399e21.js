f6327da935fb48193e83defb3e99e35ac47f9b54
Name-Javi <62718685+Name-Javi@users.noreply.github.com>
2021-04-17 00:53:55 -0500

    ....

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -84234,7 +84234,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64313" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64413" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
